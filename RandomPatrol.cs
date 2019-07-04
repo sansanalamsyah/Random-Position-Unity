5d3baa1a3ccf0dc064b66dcb0cd033da3ae3ebde
Sansan Alamsyah <51810160+sansanalamsyah@users.noreply.github.com>
2019-07-04 12:50:08 +0700

    Upload file

---
+++ RandomPatrol.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPatrol : MonoBehaviour
+{
+
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+    public float speed;
+
+    Vector2 targetPosition;
+
+    void Start()
+    {
+        targetPosition = GetRandomPosition();     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Vector2)transform.position != targetPosition)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        }
+        else
+        {
+            targetPosition = GetRandomPosition();
+        }
+    }
+
+    Vector2 GetRandomPosition()
+    {
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxX);
+        return new Vector2(randomX, randomY);
+    }
+}
